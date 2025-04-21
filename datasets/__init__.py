fd536809d5fe47a7da1bf5d07e8c1f094f80333d
Liangjing Shao <91866296+BaymaxShao@users.noreply.github.com>
2025-04-21 20:54:53 +0800

    Delete datasets directory

--- datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-#from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset
-from .scared_dataset import SCAREDRAWDataset
-from .hamlyn_dataset import HamlynDataset
-from .simcol_dataset import SimColRAWDataset
-from .servct_dataset import SERVCTDataset
-from .c3vd_dataset import C3VDDataset
